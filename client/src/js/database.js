1e86f9a9915200a881faee0fbae7fd95874d17af
GuiltyGamingJay <jerontai07@gmail.com>
2023-01-27 02:01:06 -0500

    npm i production fixed and install button working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,37 +13,28 @@ const initdb = async () =>
   });
 
 // found in classwork 26- stu manifest file
-// 
-export const putDb = async (id, content) => {
- console.log('PUT to the database');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, main: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data saved to the database', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
   console.log('GET from the database');
-   const jateDb = await openDB('jate', 1);
-   const tx = jateDb.transaction('jate', 'readonly');
-   const store = tx.objectStore('jate');
-   const request = store.getAll();
-   const result = await request;
-   console.log('result.value', result);
-   return result;
- };
-
- export const postDb = async (content) => {
-  console.log('Post to the database');
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.add({ jate: content });
+  const request = store.get(1);
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  return result?.value;
 };
 
 initdb();
